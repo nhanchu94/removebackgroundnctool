36369a109ce176bdeccb4e24b0c56de8479cf841
TiennDungg1606 <chudungfc@gmail.com>
2026-01-29 08:40:50 +0700

    gf

--- next.config.ts
+++ next.config.ts
@@ -3,14 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  async rewrites() {
-    return [
-      {
-        source: '/api/proxy-remove-bg',
-        destination: 'https://sdk.photoroom.com/v1/segment',
-      },
-    ];
-  },
 };
 
 export default nextConfig;
